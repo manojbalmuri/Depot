bd57b54dbfc7f0b067e072a3c775865983405d43
Manoj Balmuri <manoj.balmuri@gmail.com>
2012-02-28 17:12:34 +0530

    add checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+ PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
 end
+
--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -10,8 +10,18 @@ class OrdersControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:orders)
   end
+  
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'your cart is empty'
+  end
 
   test "should get new" do
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(:cart => cart, :product => products(:ruby))
+    
     get :new
     assert_response :success
   end
