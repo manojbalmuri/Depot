bf5cb9b809efef2ed5691f73bae611c75a1be05b
Manoj Balmuri <manoj.balmuri@gmail.com>
2012-02-29 12:54:20 +0530

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Depot::Application.routes.draw do
   resources :carts
 
   get "store/index"
-
-  resources :products
+  
+  resources :products do
+    get :who_bought, :on => :member
+  end
   
   #...
   
